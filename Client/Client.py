5d13345a379cbf64ea06fa852e0a663cf774a447
Stonepreheim <Stonepreheim1@gmail.com>
2021-09-23 11:39:43 -0500

    Cleaned up code
    made code more manageable

--- Client/Client.py
+++ Client/Client.py
@@ -1,14 +1,8 @@
 #written By Stone Preheim
 import socket
 
-HOST = '23.123.182.6'#my public facing IPv4
-PORT = 7771
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-    soc.connect((HOST, PORT))
-    soc.sendall(b'this should come back to me')#casting to byte like object and sending packet
-    data = soc.recv(1024)
-
-print('Received', repr(data))
+HOST = '23.123.182.6'#my public facing IPv4, nobody DDOS plz if i share in class lol
+PORT = 7771 #only open when im running server
 
 #setting up class structure for later after socket testing
 class Client:
@@ -16,3 +10,14 @@ class Client:
         print("Will write this later gonna play with sockets")
     def connectToServer(self):
         print("Starting Server: ")
+
+    def sendTestPacket(HOST, PORT):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
+            soc.connect((HOST, PORT))
+            soc.sendall(b'this should come back to me')  # casting to byte like object and sending packet
+            data = soc.recv(1024)
+
+        print('Received', repr(data))
+
+if __name__ == '__main__':
+    Client.sendTestPacket(HOST, PORT)
--- Server/Server.py
+++ Server/Server.py
@@ -3,19 +3,6 @@ import socket
 
 HOST = '192.168.1.77'#portforwarding FTW!
 PORT = 7771
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as soc:#creating socket with ipv4 and tcp. Need to reconfigure this to not use blocking.
-    soc.bind((HOST, PORT))
-    soc.listen()
-    print("server is listening.... quietly...")
-    conn, address = soc.accept()
-    with conn:
-        print("Connected to:", address)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print("Received: " + repr(data))
-            conn.sendall(data)
 
 #setting up class structure for later after socket testing
 class Server:
@@ -23,3 +10,23 @@ class Server:
         print("Will write this later gonna play with sockets")
     def startServer(self):
         print("Starting Server: ")
+
+    def testSocket(HOST, PORT):
+        with socket.socket(socket.AF_INET,
+                           socket.SOCK_STREAM) as soc:  # creating socket with ipv4 and tcp. Need to reconfigure this to not use blocking.
+            soc.bind((HOST, PORT))
+            soc.listen()
+            print("server is listening.... quietly...")
+            conn, address = soc.accept()
+            with conn:
+                print("Connected to:", address)
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print("Received: " + repr(data))
+                    conn.sendall(data)
+
+if __name__ == '__main__':
+    #controller = Server(HOST, PORT)
+    Server.testSocket(HOST, PORT)
