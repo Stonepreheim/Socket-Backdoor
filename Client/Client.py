1ad48195de631e29009f321ae62c2e58333ef52a
Stonepreheim <Stonepreheim1@gmail.com>
2021-09-23 20:29:08 -0500

    minor code changes for pres

--- Client/Client.py
+++ Client/Client.py
@@ -1,24 +1,27 @@
 #written By Stone Preheim
 import socket
 
-HOST = '23.123.182.6'#my public facing IPv4, nobody DDOS plz if i share in class lol
+ADDR = '23.123.182.6'#my public facing IPv4. If you are looking to test this dr. use loopback.
 PORT = 7771 #only open when im running server
 
 #setting up class structure for later after socket testing
 class Client:
     def __init__(self, addr, port):
-        print("Will write this later gonna play with sockets")
+        self.ADDRESS = addr
+        self.PORT = port
+
     def connectToServer(self):
         print("Attempting Connection: ")
+        #need to learn about sockets and write socket tests will come back to this
 
-    def sendTestPacket(HOST, PORT):
+    def sendTestPacket(ADDR, PORT):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-            soc.connect((HOST, PORT))
+            soc.connect((ADDR, PORT))
             soc.sendall(b'this should come back to me')  # casting to byte like object and sending packet
             data = soc.recv(1024)
 
-        print('Received', repr(data))
+        print('Server sent:', repr(data))
 
 if __name__ == '__main__':
-    #client = Client(HOST, PORT)
-    Client.sendTestPacket(HOST, PORT)
+    #client = Client(ADDR, PORT)
+    Client.sendTestPacket(ADDR, PORT)
--- Server/Server.py
+++ Server/Server.py
@@ -1,20 +1,24 @@
 #Written by Stone Preheim
 import socket
 
-HOST = '192.168.1.77'#portforwarding FTW!
+ADDR = '192.168.1.77'#portforwarding FTW! if you are looking to test this dr. use loopback
 PORT = 7771
 
 #setting up class structure for later after socket testing
 class Server:
     def __init__(self, addr, port):
-        print("Will write this later gonna play with sockets")
-    def startServer(self):
-        print("Starting Server: ")
+        self.ADDRESS = addr
+        self.PORT = port
 
-    def testSocket(HOST, PORT):
+    def startServer(self):
+        print("Starting Server...")
+        #need to learn about sockets and write socket tests will come back to this
+
+
+    def testSocket(ADDR, PORT):
         with socket.socket(socket.AF_INET,
-                           socket.SOCK_STREAM) as soc:  # creating socket with ipv4 and tcp. Need to reconfigure this to not use blocking.
-            soc.bind((HOST, PORT))
+                           socket.SOCK_STREAM) as soc:  #creating socket with ipv4 and tcp. Need to reconfigure this to not use blocking.
+            soc.bind((ADDR, PORT))
             soc.listen()
             print("server is listening.... quietly...")
             conn, address = soc.accept()
@@ -24,9 +28,9 @@ class Server:
                     data = conn.recv(1024)
                     if not data:
                         break
-                    print("Received: " + repr(data))
+                    print("Client sent:" + repr(data))
                     conn.sendall(data)
 
 if __name__ == '__main__':
-    #serv = Server(HOST, PORT)
-    Server.testSocket(HOST, PORT)
+    #serv = Server(ADDR, PORT)
+    Server.testSocket(ADDR, PORT)
